f0b3924c50ed84be594e82f7be3e79b20df35168
vittret <68021579+vittret@users.noreply.github.com>
2023-01-31 16:02:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ var discountPrice = {
   });
 
 
-  const images = ['/images/duvet-covet-turquoise.jpeg', '/images/dct2.jpg', '/images/dct3.jpg'];
+  const images = ['images/duvet-covet-turquoise.jpeg', 'images/dct2.jpg', 'images/dct3.jpg'];
       let currentImageIndex = 0;
 
       function plusSlides(n) {
@@ -45,7 +45,7 @@ var discountPrice = {
 
       /* Mobile version*/
 
-      const imagesM = ['/images/dct1-mobile.jpg', '/images/dct2-mobile.jpg', '/images/dct3-mobile.jpg'];
+      const imagesM = ['images/dct1-mobile.jpg', 'images/dct2-mobile.jpg', 'images/dct3-mobile.jpg'];
       let currentMobileImageIndex = 0;
 
       function plusSlidesM(n) {
